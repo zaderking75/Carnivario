acce6ace8271564ad1ab676cd922e93ac5f6088c
VoodoooQ <maximi.diazc@duocuc.cl>
2026-09-11 00:30:33 -0300

    Actualiza UserDetailsService y AdminDashboard

--- backend/src/main/java/duocuc/cl/rodrigo/carniverocrud/service/UserDetailsServiceImpl.java
+++ backend/src/main/java/duocuc/cl/rodrigo/carniverocrud/service/UserDetailsServiceImpl.java
@@ -2,11 +2,12 @@ package duocuc.cl.rodrigo.carniverocrud.service;
 import duocuc.cl.rodrigo.carniverocrud.repository.UsuarioDB;
 import duocuc.cl.rodrigo.carniverocrud.repository.UsuarioJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import java.util.Collections;
 
 @Service
@@ -23,7 +24,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         return new org.springframework.security.core.userdetails.User(
                 usuario.getEmail(),
                 usuario.getPassword(),
-                Collections.emptyList()
+                Collections.singletonList(
+    new SimpleGrantedAuthority("ROLE_" + usuario.getRole()))
         );
     }
 }
